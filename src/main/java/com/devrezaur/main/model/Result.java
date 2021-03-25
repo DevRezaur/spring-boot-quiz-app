34f4e3f43ec694d4c5363bfa26b15886ca2841a3
Rezaur Rahman <rezaur.official@gmail.com>
2021-03-25 20:24:59 +0600

    Added: Onclick to result page

--- src/main/java/com/devrezaur/main/model/Result.java
+++ src/main/java/com/devrezaur/main/model/Result.java
@@ -4,10 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import org.springframework.stereotype.Component;
 
 @Component
 @Entity
+@Table(name = "results")
 public class Result {
 
 	@Id
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-insert into Question(ques_id, title, optionA, optionB, optionC, ans, chose)
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(1, "Question 1",  "Option 1", "Option 2", "Option 3", 1, -1);
 
-insert into Question(ques_id, title, optionA, optionB, optionC, ans, chose)
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(2, "Question 2",  "Option 1", "Option 2", "Option 3", 2, -1);
 
-insert into Question(ques_id, title, optionA, optionB, optionC, ans, chose)
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(3, "Question 3",  "Option 1", "Option 2", "Option 3", 3, -1);
 
-insert into Question(ques_id, title, optionA, optionB, optionC, ans, chose)
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(4, "Question 4",  "Option 1", "Option 2", "Option 3", 3, -1);
